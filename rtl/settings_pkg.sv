182b2c2d397997acf6cc16cafc9b06977f064204
Alina Ivanova <alina.al.ivanova@gmail.com>
2016-11-30 19:33:01 +0300

    Add include functions_pkg

--- rtl/settings_pkg.sv
+++ rtl/settings_pkg.sv
@@ -31,10 +31,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //-----------------------------------------------------------------------------
-//`include "functions_pkg.sv"
+`include "functions_pkg.sv"
 //-----------------------------------------------------------------------------
 package settings_pkg;
-	import functions_pkg::*; 
+    import functions_pkg::*; 
 //-----------------------------------------------------------------------------
 // Parameter Declaration(s)
 //-----------------------------------------------------------------------------
